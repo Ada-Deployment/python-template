5e5e20c3fa76f8e1f0bd6418fb7b5e0b4f8eaa73
samaravazquezperez <samaravazquezperez@ada-mode.com>
2021-07-08 12:54:15 +0000

    Automated autoyapf fixes

--- application.py
+++ application.py
@@ -1,13 +1,13 @@
-from flask import Flask, app,jsonify
+from flask import Flask, app, jsonify
 
 application = app = Flask(__name__)
 
 
-@app.route('/', methods = ['GET', 'POST']) #landing page redirects to the sign in page
+@app.route('/', methods=['GET',
+                         'POST'])  #landing page redirects to the sign in page
 def index():
     return jsonify("Testing new blue/green deployment")
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port = 80)
-
+    app.run(debug=True, host='0.0.0.0', port=80)
